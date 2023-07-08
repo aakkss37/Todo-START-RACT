fc0ae4e9f65eb94c44ee3aa49137b1a2ebed3561
amar kumar <amarkumar.sharma.124@gmail.com>
2023-07-09 02:16:10 +0530

    todo style complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
 import "./appStyle.css";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { MdOutlineDoneOutline } from "react-icons/md";
+
 function App() {
+	const [userInput, setUserInput] = useState("");
+
+	const updateUserInputState = () => {
+		console.log("clicked");
+		setUserInput("sneha i love u " + Math.random() * 10000);
+	};
+
 	return (
 		<>
 			<div className="app_container">
@@ -13,13 +24,24 @@ function App() {
 				</div>
 				<div className="input_container">
 					<input type="text" placeholder="What u gonna do today" />
-					<button className="input_container_add">+</button>
+					<button
+						className="input_container_add"
+						onClick={updateUserInputState}>
+						+
+					</button>
 				</div>
 				<div className="todo_items_container">
 					<div className="todo_item">
-						<p>List item 1</p>
-						<button>Delete</button>
-						<button>Done</button>
+						<p className="todo_item_tex">Pending</p>
+						<div className="todo_item_action_button_container">
+							<button>
+								<RiDeleteBin6Line />
+							</button>
+							<button>
+								<MdOutlineDoneOutline />
+								{/* <FaUndo /> */}
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
